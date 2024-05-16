e098cd1439269b6edf89f3bf07295c00be9de9ed
github_username <you@example.com>
2024-05-16 18:40:02 +0700

    CORS and IIS Configuration

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using WebApp.ServiceExtensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -17,6 +22,18 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Enables using static files for the request
+// If not set a path to the static files directory
+// It will use a wwwroot folder in our project by default
+app.UseStaticFiles();
+
+// Forward proxy headers to the current request
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
--- WebApp/ServiceExtensions/ServiceExtensions.cs
+++ WebApp/ServiceExtensions/ServiceExtensions.cs
@@ -2,14 +2,20 @@
 
 public static class ServiceExtensions
 {
+    // Mechanism to give or restrict access rights to applications from different domains
     public static void ConfigureCors(this IServiceCollection services) => services.AddCors(options =>
     {
         options.AddPolicy("CorsPolicy", builder =>
             builder.AllowAnyOrigin() // allows requests from any source
-                                     // Or WithOrigins("https://example.com") 
+                                     // Or WithOrigins("https://example.com")
             .AllowAnyMethod() // allows all HTTP methods
                               // Or WithMethods("POST", "GET")
             .AllowAnyHeader()); // allow all headers
                                 // WithHeaders("accept", "contenttype")
+    });
+
+    // Host our application on IIS for deployment
+    public static void ConfigureIISIntegration(this IServiceCollection services) => services.Configure<IISOptions>(options =>
+    {
     });
 }
