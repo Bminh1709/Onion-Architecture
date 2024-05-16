8229e69e149363ea8e5562db78985e0b6abdac48
github_username <you@example.com>
2024-05-16 22:15:01 +0700

    Handle First Get Method

--- Contracts/ICompanyRepository.cs
+++ Contracts/ICompanyRepository.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Entities.Models;
 
 namespace Contracts;
 
 public interface ICompanyRepository
 {
+    IEnumerable<Company> GetAllCompanies(bool trackChanges);
 }
--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -13,4 +13,9 @@ public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
     public CompanyRepository(RepositoryContext repositoryContext) : base(repositoryContext)
     {
     }
+
+    public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
+        FindAll(trackChanges)
+        .OrderBy(c => c.Name)
+        .ToList();
 }
