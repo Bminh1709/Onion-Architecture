5a117ef908b38a55d776a3b203519cc2408a46de
github_username <you@example.com>
2024-05-16 18:22:56 +0700

    Add project files.

---
+++ WebApp/ServiceExtensions/ServiceExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApp.ServiceExtensions;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureCors(this IServiceCollection services) => services.AddCors(options =>
+    {
+        options.AddPolicy("CorsPolicy", builder =>
+            builder.AllowAnyOrigin() // allows requests from any source
+                                     // Or WithOrigins("https://example.com") 
+            .AllowAnyMethod() // allows all HTTP methods
+                              // Or WithMethods("POST", "GET")
+            .AllowAnyHeader()); // allow all headers
+                                // WithHeaders("accept", "contenttype")
+    });
+}
