3a022f3c3d252465f7a700fa6c01e10b5200650c
github_username <you@example.com>
2024-05-17 12:29:12 +0700

    Update Employee

--- Shared/DataTransferObjects.cs
+++ Shared/DataTransferObjects.cs
@@ -6,5 +6,6 @@
         public record EmployeeDto(Guid Id, string Name, int Age, string Position);
         public record CompanyForCreationDto(string Name, string Address, string Country, IEnumerable<EmployeeForCreationDto>? Employees);
         public record EmployeeForCreationDto(string Name, int Age, string Position);
+        public record EmployeeForUpdateDto(string Name, int Age, string Position);
     }
 }
