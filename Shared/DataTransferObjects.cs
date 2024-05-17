ded20dd62ae4959ee49eb58453610228e2337727
github_username <you@example.com>
2024-05-17 10:07:35 +0700

    Create Employee

--- Shared/DataTransferObjects.cs
+++ Shared/DataTransferObjects.cs
@@ -5,5 +5,6 @@
         public record CompanyDto(Guid Id, string Name, string FullAddress);
         public record EmployeeDto(Guid Id, string Name, int Age, string Position);
         public record CompanyForCreationDto(string Name, string Address, string Country);
+        public record EmployeeForCreationDto(string Name, int Age, string Position);
     }
 }
